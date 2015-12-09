7decc056f94fb6ba98f54969103ed831f88acfba
Nguyen Xuan Tuong <nguy0066@e.ntu.edu.sg>
2015-12-09 12:24:42 +0800

    kickstart

---
+++ config.h
@@ -0,0 +1,81 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Debug Mode */
+/* #undef DEBUG */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <iostream> header file. */
+#define HAVE_IOSTREAM 1
+
+/* Define to 1 if you have the `gflags' library (-lgflags). */
+#define HAVE_LIBGFLAGS 1
+
+/* Define to 1 if you have the `glog' library (-lglog). */
+#define HAVE_LIBGLOG 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <string> header file. */
+#define HAVE_STRING 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* No-debug Mode */
+#define NDEBUG /**/
+
+/* Name of package */
+#define PACKAGE "rtbpack"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "nguy0066@e.ntu.edu.sg"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "rtbpack"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "rtbpack 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "rtbpack"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.1"
---
+++ test/hello_test.cpp
@@ -0,0 +1,22 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <list>
+
+#include <gtest/gtest.h>
+//#include <folly/json.h>
+//#include <boost/next_prior.hpp>
+
+//using folly::dynamic;
+//using folly::parseJson;
+//using folly::toJson;
+
+TEST(Hello, HelloFirst) {
+    int n = 0;
+    EXPECT_EQ(0, n);
+}
+
+//TEST(Hello, TestJSON) {
+//    auto num = parseJson("12");
+//    EXPECT_TRUE(num.isInt());
+//}
